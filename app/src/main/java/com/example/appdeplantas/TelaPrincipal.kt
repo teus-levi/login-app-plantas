7d2714ec68922b0c455e088cc04c9ab70b57c036
teus-levi <mateusdias2001@gmail.com>
2024-10-06 23:12:20 -0300

    inicial

---
+++ app/src/main/java/com/example/appdeplantas/TelaPrincipal.kt
@@ -0,0 +1,24 @@
+package com.example.appdeplantas
+
+import android.graphics.Color
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class TelaPrincipal : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_tela_principal)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        //supportActionBar!!.hide()
+        window.statusBarColor = Color.parseColor("#00a86b")
+    }
+}
